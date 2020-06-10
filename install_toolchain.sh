d8cf30e89545339c826028d38796bfa39efb172a
KarasawaTakahiro <karasawa@basicinc.jp>
2020-06-10 12:51:02 +0900

    gem 追加

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -1,3 +1,3 @@
 if which ruby ; then
-  gem install --no-doc solargraph rubocop rubocop-performance rubocop-rails rubocop-rspec
+  gem install --no-doc solargraph rubocop rubocop-performance rubocop-rails rubocop-rspec neovim ec2ssh
 fi
